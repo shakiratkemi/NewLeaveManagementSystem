a6b63e8be68b2a52ed8432eb3717e01153dd2989
shakirat <shakiratganiyy@gmail.com>
2026-08-10 15:27:34 +0100

    Updated add-employee.ts

--- src/app/pages/hr/employees/add-employee/add-employee.ts
+++ src/app/pages/hr/employees/add-employee/add-employee.ts
@@ -33,7 +33,7 @@ export class AddEmployee {
       department: ['Engineering', Validators.required],
       designation: ['', Validators.required],
       role: ['Employee', Validators.required],
-      clientResetUrl: [`${window.location.origin}/auth/reset-password`, Validators.required],
+      clientResetUrl: [`${window.location.origin}/reset-password`, Validators.required],
     });
   }
 
